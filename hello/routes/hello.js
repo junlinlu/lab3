ec2b04fc281e859719bb73cda33048f623a619a3
Junlin Lu <jul223@ucsd.edu>
2019-01-23 23:12:14 -0800

    update node js

--- hello/routes/hello.js
+++ hello/routes/hello.js
@@ -3,8 +3,10 @@
  * GET home page.
  */
 
-exports.view = function(req, res){
+exports.view = function(req, res){ //(request,result)
+  var nameToShow = req.params.userName; //get parameter from request, username is in the params object
+  console.log("name is " + nameToShow);
   res.render('index', {
-  	'name': '',
+    'name': nameToShow,
   });
 };
